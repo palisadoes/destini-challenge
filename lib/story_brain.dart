23225a35a2fe3ee0d3a923c5b8ebb65ae1f23d21
Peter Harrison <blackriver@simiya.com>
2022-01-04 21:06:05 -0800

    Completed Step 25

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -4,7 +4,7 @@ import 'story.dart';
 //TODO: Step 5 - Create a new class called StoryBrain.
 class StoryBrain {
   //TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-  int storyNumber = 0;
+  int _storyNumber = 0;
 
   //TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
   List<Story> _storyData = [
@@ -41,21 +41,21 @@ class StoryBrain {
 
   //TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[storyNumber].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[storyNumber].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[storyNumber].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 
   //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
@@ -63,19 +63,19 @@ class StoryBrain {
     //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
     //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
     //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-    if ((storyNumber == 0) && (choiceNumber == 1)) {
-      storyNumber = 2;
-    } else if ((storyNumber == 0) && (choiceNumber == 2)) {
-      storyNumber = 1;
-    } else if ((storyNumber == 1) && (choiceNumber == 1)) {
-      storyNumber = 2;
-    } else if ((storyNumber == 1) && (choiceNumber == 2)) {
-      storyNumber = 3;
-    } else if ((storyNumber == 2) && (choiceNumber == 1)) {
-      storyNumber = 5;
-    } else if ((storyNumber == 2) && (choiceNumber == 2)) {
-      storyNumber = 4;
-    } else if ([3, 4, 5].contains(storyNumber)) {
+    if ((_storyNumber == 0) && (choiceNumber == 1)) {
+      _storyNumber = 2;
+    } else if ((_storyNumber == 0) && (choiceNumber == 2)) {
+      _storyNumber = 1;
+    } else if ((_storyNumber == 1) && (choiceNumber == 1)) {
+      _storyNumber = 2;
+    } else if ((_storyNumber == 1) && (choiceNumber == 2)) {
+      _storyNumber = 3;
+    } else if ((_storyNumber == 2) && (choiceNumber == 1)) {
+      _storyNumber = 5;
+    } else if ((_storyNumber == 2) && (choiceNumber == 2)) {
+      _storyNumber = 4;
+    } else if ([3, 4, 5].contains(_storyNumber)) {
       restart();
     }
   }
